9698bf61a1ebf4999e80779e1d8a22faa0e37913
Vladislav <vladislafff10@gmail.com>
2022-09-07 18:40:30 +0300

    Add details for country

--- src/pages/Details.jsx
+++ src/pages/Details.jsx
@@ -1,10 +1,30 @@
-import React from 'react'
+import axios from "axios"
+import { useEffect, useState } from "react"
+import { IoArrowBack } from "react-icons/io5"
+import { useNavigate, useParams } from "react-router-dom"
+import { searchByCountry } from "../config";
+import { Button } from "../components/Button"
+import Info from "../components/Info";
 
 const Details = () => {
+	const { name } = useParams();
+	const navigate = useNavigate();
+	const goBack = () => navigate(-1);
+	const push = (neighbor) => navigate(`/country/${neighbor}`)
 
+	const [country, setCountry] = useState(null);
+
+	useEffect(() => {
+		axios.get(searchByCountry(name)).then(
+			({ data }) => setCountry(data[0])
+		)
+	}, [name])
 	return (
 		<div>
-			Details
+			<Button onClick={goBack}>
+				<IoArrowBack /> Back
+			</Button>
+			{country && <Info push={push} {...country} />}
 		</div>
 	)
 }
